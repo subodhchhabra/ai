c5cda64d636ad6fb63d3624ff20a11191c153f84
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2019-03-03 12:14:41 +0100

    insurance example moved to a repo of itself

--- docs/js/insurance/predict.js
+++
@@ -1,8 +0,0 @@
-(async () => {
-  // const model = await tf.loadLayersModel('localstorage://insurance');
-  const model = await tf.loadLayersModel('indexeddb://insurance');
-  // 0 - red: many accidents
-  // 1 - green: few or no accidents
-  // 2 - yellow: in the middle
-  model.predict(tf.tensor([[48, 100, 10]])).print();
-})();
